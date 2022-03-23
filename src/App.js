2b278661c67fd9b090f5afa0839f8084eefdc760
ghost_32 <kalash.strt@gmail.com>
2022-03-23 19:18:43 +0530

    feat: routes are added with temporary components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+// importing components
+import Home from './routes/Home';
+import Login from './routes/Login';
+import Logout from './routes/Logout';
+import Menu from './routes/Menu';
 
 function App() {
   return (
     <div className="App">
-      <h1>Kalash Sharma</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='/menu' element={<Menu />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/routes/Logout.js
@@ -0,0 +1,5 @@
+function Logout(){
+    return <h1>Logout</h1>
+}
+
+export default Logout;
