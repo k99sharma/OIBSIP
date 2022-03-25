bf971d6bb8bc4de20952d716aa6f9a79d7ef7aaf
ghost_32 <kalash.strt@gmail.com>
2022-03-25 19:40:39 +0530

    feat: routing is added

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,9 +1,36 @@
+// imports
+import Link from "next/link";
+
 export default function Navbar(){
     return(
-        <div>
-            <strong>
-                Mount Pizza
-            </strong>
-        </div>
+        <>
+            <div>
+                <strong>
+                    Mount Pizza
+                </strong>
+            </div>
+            <br />
+            <div>
+                <ul>
+                    <li>
+                        <Link href='/signup'>
+                            Signup
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href='/login'>
+                            Login
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href='menu'>
+                            Menu
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </>    
     )
 }
---
+++ pages/signup/index.js
@@ -0,0 +1,5 @@
+export default function Signup(){
+    return(
+        <h1>Signup</h1>
+    )
+}
