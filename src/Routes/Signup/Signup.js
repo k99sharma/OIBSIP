bf2da626b4ff10b24e9eb5b6047d4fa04b0994d5
ghost_32 <kalash.strt@gmail.com>
2022-03-28 00:10:12 +0530

    fix: signup route is fixed

--- src/Routes/Signup/Signup.js
+++ src/Routes/Signup/Signup.js
@@ -27,97 +27,13 @@ export default function Signup() {
         if (res.error) {
             alert(res.message);
         } else {
-            authCtx.login(res.data);
-            navigator('/', { replace: true }); // redirect user afer sign up
+            alert('Signup Successful');
+            navigator('/login', { replace: true }); // redirect user afer sign up
         }
     }
 
     return (
         <>
-            {/* <div>
-                Signup Page
-                <p>Already got an account? <Link to='/login'>Login</Link></p>
-            </div><br />
-
-            <form onSubmit = {handleSubmit} autoComplete="off">
-                <div>
-                    <label htmlFor="fname">
-                        First Name
-                    </label><br />
-                    <input 
-                        type='text' 
-                        id='fname'
-                        placeholder="Enter First Name" 
-                        onChange = {e => setFirstName(e.target.value)}
-                        name='fname' 
-                        required 
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor="lname">
-                        Last Name
-                    </label><br />
-                    <input 
-                        type='text' 
-                        id='lname'
-                        placeholder="Enter Last Name" 
-                        onChange = {e => setLastName(e.target.value)}
-                        name='lname' 
-                        required 
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor="email">
-                        Email Address
-                    </label><br />
-                    <input 
-                        type='email' 
-                        id='email'
-                        placeholder="Enter Email Address" 
-                        onChange = {e => setEmail(e.target.value)}
-                        name='email'
-                        required 
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor="phoneNumber">
-                        Phone Number
-                    </label><br />
-                    <input 
-                        type='text' 
-                        id='phoneNumber'
-                        placeholder="Enter Phone Number" 
-                        onChange = {e => setPhoneNumber(e.target.value)}
-                        name='phoneNumber' 
-                        required 
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor="password">
-                        Password
-                    </label><br />
-                    <input 
-                        type='password' 
-                        id='password'
-                        placeholder="Enter Password"
-                        onChange = {e => setPassword(e.target.value)} 
-                        name='password' 
-                        required 
-                    />
-                </div>
-
-                <div>
-                    <button type='submit'>
-                        Signup
-                    </button>
-                </div>
-            </form> */}
-
-
             <div className="w-full h-4/5 grid grid-cols-1 md:grid-cols-2 justify-items-center items-center">
                 <div className="signupForm rounded-2xl w-11/12 md:w-8/12 p-5">
                     <div className="signupForm__title text-5xl font-medium">
