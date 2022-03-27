959edc67f1552115a2ba9a92f2291006833cc46f
ghost_32 <kalash.strt@gmail.com>
2022-03-27 20:43:46 +0530

    add: contact me form is added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -54,6 +54,41 @@ function FollowMe() {
     )
 }
 
+function Contact() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <>
+            <div className='footer__contact my-5'>
+                <form onSubmit={handleSubmit} autoComplete='off'>
+                    <div className='flex'>
+                        <div>
+                            <input className='p-2 rounded-l-md' type='text' name='contactme' placeholder='Enter your email address' />
+                        </div>
+                        <button type='submit' className='bg-amber-400 rounded-r-md p-2'>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+                            </svg>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+}
+
+function Copyright() {
+    return (
+        <>
+            <div className='footer__copyright text-sm text-white'>
+                © 2022 Mount Pizza. All rights reserved.
+            </div>
+        </>
+    );
+}
+
 export function Footer() {
     return (
         <>
@@ -64,10 +99,10 @@ export function Footer() {
                     <FollowMe />
                 </div>
 
-                <div className='footer__right'>
-                    <div className='footer__copyright text-sm text-white'>
-                        © 2022 Mount Pizza. All rights reserved.
-                    </div>
+                <div className='footer__right flex items-center justify-items-center'>
+                    <Contact />
+
+                    <Footer />
                 </div>
             </div>
         </>
