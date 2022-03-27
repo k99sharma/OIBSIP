012ebff1b000ac04b8bd799cb2edad66d72a10b1
ghost_32 <kalash.strt@gmail.com>
2022-03-27 19:09:22 +0530

    feat: footer is created

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,75 @@
-export function Footer()
-{
-    return(
+import './Footer.css';
+
+function CompanyName() {
+    return (
         <>
-            Footer
+            <div className='company flex items-center justify-items-center my-2'>
+                <div className='footer__company__logo mx-2'>
+                    <img src='/logo512.png' alt='logo' width='50px' height='50px' />
+                </div>
+
+                <div className='footer__company__name mx-2 text-white font-medium'>
+                    Mount Pizza
+                </div>
+            </div>
+        </>
+    )
+}
+
+function FollowMe() {
+    return (
+        <>
+            <div className='footer__followme mt-5'>
+                <div className='text-white mb-3'>
+                    Developed & Designed by <span>
+                        <a className='text-amber-400' href="https://www.kalashsharma.me">
+                            ghost_32
+                        </a>
+                    </span>
+                </div>
+            </div>
+
+            <div className='footer__followme__links flex justify-center items-center py-3'>
+                <div className='flex'>
+                    <div className='mx-2'>
+                        <a href="https://www.linkedin.com/in/kalashsharma99/" target='_blank'>
+                            <img src="/linkedin.png" width="25px" height="25px" alt="linkedin" />
+                        </a>
+                    </div>
+
+                    <div className='mx-2'>
+                        <a href="https://github.com/k99sharma" target='_blank'>
+                            <img src="/github.png" width="25px" height="25px" alt="github" />
+                        </a>
+                    </div>
+
+                    <div className='mx-2'>
+                        <a href="https://www.kalashsharma.me" target='_blank'>
+                            <img src="/web.png" width="25px" height="25px" alt="portfolio" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export function Footer() {
+    return (
+        <>
+            <div className="footer grid grid-cols-1 items-center justify-items-start md:grid-cols-2 bg-black py-10 px-28">
+                <div className='footer__left'>
+                    <CompanyName />
+
+                    <FollowMe />
+                </div>
+
+                <div className='footer__right'>
+                    <div className='footer__copyright text-sm text-white'>
+                        © 2022 Mount Pizza. All rights reserved.
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/components/Navbar/AuthLink/AuthLink.js
+++ src/components/Navbar/AuthLink/AuthLink.js
@@ -45,6 +45,9 @@ export function AuthLink(props) {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                         </svg>
                     </div>
+                    <div className="ml-2">
+                        Logout
+                    </div>
                 </div>
             }
         </>
